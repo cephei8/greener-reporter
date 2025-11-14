d5efde96f11d3b4a4b02116b74b11d10a0572c62
cephei8 <208798380+cephei8@users.noreply.github.com>
2025-11-15 00:39:40 +0100

    Rewrite servermock in Zig

--- tests-ffi/build.rs
+++ tests-ffi/build.rs
@@ -3,45 +3,17 @@ use std::path::PathBuf;
 
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
-    let target = std::env::var("TARGET").unwrap();
 
-    let target_dir_with_target_rs = PathBuf::from(&manifest_dir)
-        .join("..")
-        .join("target")
-        .join(&target)
-        .join(&profile);
-
-    let target_dir_rs = if target_dir_with_target_rs.exists() {
-        target_dir_with_target_rs
-    } else {
-        PathBuf::from(&manifest_dir)
-            .join("..")
-            .join("target")
-            .join(&profile)
-    };
-
-    let target_dir_zig = PathBuf::from(&manifest_dir)
+    let target_dir = PathBuf::from(&manifest_dir)
         .join("..")
         .join("zig-out")
         .join("lib");
 
-    println!("cargo:rustc-link-search=native={}", target_dir_rs.display());
-    println!(
-        "cargo:rustc-link-search=native={}",
-        target_dir_zig.display()
-    );
+    println!("cargo:rustc-link-search=native={}", target_dir.display());
     println!("cargo:rustc-link-lib=dylib=greener_reporter");
     println!("cargo:rustc-link-lib=dylib=greener_servermock");
 
-    println!(
-        "cargo:rustc-link-arg=-Wl,-rpath,{}",
-        target_dir_rs.display()
-    );
-    println!(
-        "cargo:rustc-link-arg=-Wl,-rpath,{}",
-        target_dir_zig.display()
-    );
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", target_dir.display());
 
     let include_dir = PathBuf::from(&manifest_dir).join("../include");
     let reporter_header = include_dir.join("greener_reporter/greener_reporter.h");
